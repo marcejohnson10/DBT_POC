bd3f9d0a5f83383951b201c0f2f056d7444422af
marc.johnson@53.com <marc.johnson@53.com>
2022-12-02 18:56:38 +0000

    codegen updates

---
+++ analysis/codegen_models_yaml.sql
@@ -0,0 +1,3 @@
+{{ codegen.generate_model_yaml(
+    model_names=['customer_rslt','lineitem_rslt','nation_incr_test_rslt3','nation_incr_test_rslt31','orders_rslt','nation_incr_test_rslt','nation_incr_test_rslt41','partsupp_rslt','nation_rslt','region_rslt','test_2','nation_incr_test_rslt2','part_rslt','test2','supplier_rslt']
+) }}
